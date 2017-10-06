5197c1c1b7ab1e9c636de33572fc401af3336c18
mutexre <alexander.obuschenko@gmail.com>
2017-10-07 05:36:32 +0700

    update

--- SceneKitGlass/GameViewController.swift
+++ SceneKitGlass/GameViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import ReplayKit
+import SnapKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, RPScreenRecorderDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,51 +34,36 @@ class GameViewController: UIViewController {
         camera.wantsHDR = true
         camera.sensorHeight = 24
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .spot
-        lightNode.light!.intensity = 500
-        lightNode.position = SCNVector3(x: 0, y: 0, z: 20)
-//        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-//        let ambientLightNode = SCNNode()
-//        ambientLightNode.light = SCNLight()
-//        ambientLightNode.light!.type = .ambient
-//        ambientLightNode.light!.color = UIColor.darkGray
-//        scene.rootNode.addChildNode(ambientLightNode)
-        
         let environment = UIImage(named: "env.jpg")
         scene.lightingEnvironment.contents = environment
         scene.lightingEnvironment.intensity = 2.0
         scene.background.contents = environment
         
-        // retrieve the ship node
         let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
         
-        // animate the 3d object
-//        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
         let scnView = self.view as! SCNView
-        
-        // set the scene to the view
         scnView.scene = scene
-        
-        // allows the user to manipulate the camera
         scnView.allowsCameraControl = true
         
-        // show statistics such as fps and timing information
-//        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
+        scnView.backgroundColor = .black
         scnView.antialiasingMode = .multisampling4X
         
-        // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
+        
+        let recordButton = UIButton()
+        scnView.addSubview(recordButton)
+        recordButton.backgroundColor = .yellow
+        recordButton.setTitleColor(.black, for: .normal)
+        recordButton.setTitle("Start Recording", for: .normal)
+        recordButton.addTarget(self, action: #selector(startRecording(_:)), for: .touchUpInside)
+        recordButton.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-20)
+        }
+        recordButton.isHidden = true
+        
+        RPScreenRecorder.shared().delegate = self
     }
     
     @objc
@@ -136,4 +123,66 @@ class GameViewController: UIViewController {
         // Release any cached data, images, etc that aren't in use.
     }
 
+    @objc func startRecording(_ sender: UIButton)
+    {
+        if RPScreenRecorder.shared().isAvailable
+        {
+            RPScreenRecorder.shared().startRecording { (error: Error?) -> Void in
+                if error == nil {
+                    print("Recording started")
+                    
+                    sender.removeTarget(self, action: #selector(self.startRecording(_:)), for: .touchUpInside)
+                    sender.addTarget(self, action: #selector(self.stopRecording(_:)), for: .touchUpInside)
+                    sender.setTitle("Stop Recording", for: .normal)
+                    sender.setTitleColor(.red, for: .normal)
+                }
+                else {
+                    print("Unable to start recording")
+                }
+            }
+        }
+        else {
+            print("Screen recording is unavailable")
+        }
+    }
+
+    @objc func stopRecording(_ sender: UIButton)
+    {
+        RPScreenRecorder.shared().stopRecording { (previewController: RPPreviewViewController?, error: Error?) -> Void in
+            if previewController != nil
+            {
+                let alertController = UIAlertController(title: "Recording", message: "Do you wish to discard or view your gameplay recording?", preferredStyle: .alert)
+                
+                let discardAction = UIAlertAction(title: "Discard", style: .default) { (action: UIAlertAction) in
+                    RPScreenRecorder.shared().discardRecording { () -> Void in
+                        // Executed once recording has successfully been discarded
+                    }
+                }
+                
+                let viewAction = UIAlertAction(title: "View", style: .default, handler: { (action: UIAlertAction) -> Void in
+                    self.present(previewController!, animated: true, completion: nil)
+                })
+                
+                alertController.addAction(discardAction)
+                alertController.addAction(viewAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+                sender.removeTarget(self, action: #selector(self.stopRecording(_:)), for: .touchUpInside)
+                sender.addTarget(self, action: #selector(self.startRecording(_:)), for: .touchUpInside)
+                sender.setTitle("Start Recording", for: .normal)
+                sender.setTitleColor(.blue, for: .normal)
+            } else {
+                // Handle error
+            }
+        }
+    }
+    
+    public func screenRecorder(_ screenRecorder: RPScreenRecorder, didStopRecordingWith previewViewController: RPPreviewViewController?, error: Error?) {
+        print("Screen recorder did stop recording (error: \(error.debugDescription)")
+    }
+    
+    public func screenRecorderDidChangeAvailability(_ screenRecorder: RPScreenRecorder) {
+        print("Screen recording did change availability")
+    }
 }
